c68fcc5afc59932f9e6cc9a78d2a9a325b4ab9b8
Randy Black <randy@blacksdesign.com>
2021-12-06 22:32:12 -0800

    Added the special field "field_validation_errors" to the API output for /items/objects and /items/versions. This shows all the validation errors (field with red backgrounds).

--- partforge/application/DBTableRowItemObject.php
+++ partforge/application/DBTableRowItemObject.php
@@ -25,25 +25,27 @@
 
 class DBTableRowItemObject extends DBTableRow {
 
-	public function __construct($ignore_joins=false,$parent_index=null) {
-		parent::__construct('itemobject',$ignore_joins,$parent_index);
-		//      $this->user_id = $_SESSION['account']->user_id;
-	}
+    public function __construct($ignore_joins = false, $parent_index = null)
+    {
+        parent::__construct('itemobject', $ignore_joins, $parent_index);
+        //      $this->user_id = $_SESSION['account']->user_id;
+    }
 
-	/**
-	 * This updates the fields itemobject.cached_last_comment_date and itemobject.cached_last_comment_person
-	 * It is used strictly for performance.   It should be called when a comment is added, edited, deleted.
-	 * If $itemobject_id is set then is only updates the cached fields for that specific itemobject_id.
-	 */
-	static public function updateCachedLastCommentFields($itemobject_id=null) {
-		
-		// if the itemobject is specified then we only create entries for this specific itemobject_id for the last_comment_tbl.
-		$inner_group_by = is_null($itemobject_id) ? "GROUP BY bb_c.itemobject_id" : "and bb_c.itemobject_id='{$itemobject_id}'";
-		
-		// if the itemobject is specified then we have to be careful to only overwrite for the specified itemobject_id rather than all of them.
-		$final_where = is_null($itemobject_id) ? '' : "WHERE cc_io.itemobject_id='{$itemobject_id}'";
-		
-		DbSchema::getInstance()->mysqlQuery("
+    /**
+     * This updates the fields itemobject.cached_last_comment_date and itemobject.cached_last_comment_person
+     * It is used strictly for performance.   It should be called when a comment is added, edited, deleted.
+     * If $itemobject_id is set then is only updates the cached fields for that specific itemobject_id.
+     */
+    static public function updateCachedLastCommentFields($itemobject_id = null)
+    {
+
+        // if the itemobject is specified then we only create entries for this specific itemobject_id for the last_comment_tbl.
+        $inner_group_by = is_null($itemobject_id) ? "GROUP BY bb_c.itemobject_id" : "and bb_c.itemobject_id='{$itemobject_id}'";
+
+        // if the itemobject is specified then we have to be careful to only overwrite for the specified itemobject_id rather than all of them.
+        $final_where = is_null($itemobject_id) ? '' : "WHERE cc_io.itemobject_id='{$itemobject_id}'";
+
+        DbSchema::getInstance()->mysqlQuery("
 			UPDATE itemobject as cc_io
 				# Table of all the last comment for each itemobject_id
 				LEFT JOIN (
@@ -56,37 +58,40 @@ class DBTableRowItemObject extends DBTableRow {
 				    WHERE max_comment_dates.max_comment_added=bb_c.comment_added
 					{$inner_group_by}
 						) as last_comment_tbl ON cc_io.itemobject_id=last_comment_tbl.bb_io
-		
+
 			SET cc_io.cached_last_comment_date=last_comment_tbl.bb_comment_added,
 				cc_io.cached_last_comment_person=TRIM(CONCAT(last_comment_tbl.bb_user_first_name,' ',last_comment_tbl.bb_user_last_name))
 			{$final_where}
 		");
-	}
-	
-	/**
-	 * Updates the fields itemobject.cached_last_ref_date and itemobject.cached_last_ref_person.
-	 * It should be called when a component is added or edited.  If $itemobject_ids are specified
-	 * then it updates the cache fields for only the specified $itemobject_ids.  It is the responsibility
-	 * of the caller to construct a list of all the itemobject_ids that might need updating.
-	 * If called without any itemobjects than it updates globally.
-	 * 
-	 * Generally when you call this, you would build a list of itemobjects that are components of item
-	 * you are editing.  You do this for both before and after cases and merge together.
-	 * 
-	 */
-	static public function updateCachedLastReferenceFields($itemobject_ids=null) {
-		if (is_array($itemobject_ids)) {
-			// We don't do anything if we have an empty array.  that means there were no refereces up to update.
-			if (count($itemobject_ids)==0) return;
-			$itemobject_ids = array_unique($itemobject_ids); // don't repeat ourselves
-			$where1 = " WHERE cc_io.itemobject_id IN (".implode(',',$itemobject_ids).")";
-			$where2 = " WHERE aa_ic.has_an_itemobject_id IN (".implode(',',$itemobject_ids).")";
-		} else {
-			$where1 = '';
-			$where2 = '';
-		}
+    }
 
-		DbSchema::getInstance()->mysqlQuery("
+    /**
+     * Updates the fields itemobject.cached_last_ref_date and itemobject.cached_last_ref_person.
+     * It should be called when a component is added or edited.  If $itemobject_ids are specified
+     * then it updates the cache fields for only the specified $itemobject_ids.  It is the responsibility
+     * of the caller to construct a list of all the itemobject_ids that might need updating.
+     * If called without any itemobjects than it updates globally.
+     *
+     * Generally when you call this, you would build a list of itemobjects that are components of item
+     * you are editing.  You do this for both before and after cases and merge together.
+     *
+     */
+    static public function updateCachedLastReferenceFields($itemobject_ids = null)
+    {
+        if (is_array($itemobject_ids)) {
+            // We don't do anything if we have an empty array.  that means there were no refereces up to update.
+            if (count($itemobject_ids)==0) {
+                return;
+            }
+            $itemobject_ids = array_unique($itemobject_ids); // don't repeat ourselves
+            $where1 = " WHERE cc_io.itemobject_id IN (".implode(',', $itemobject_ids).")";
+            $where2 = " WHERE aa_ic.has_an_itemobject_id IN (".implode(',', $itemobject_ids).")";
+        } else {
+            $where1 = '';
+            $where2 = '';
+        }
+
+        DbSchema::getInstance()->mysqlQuery("
 			UPDATE itemobject as cc_io
 				# Table of all the last referenced itemversion info for each itemobject_id
 				LEFT JOIN (
@@ -104,23 +109,24 @@ class DBTableRowItemObject extends DBTableRow {
 					  WHERE max_ref_dates.max_effective_date=bb_iv.effective_date
 					  GROUP BY bb_ic.has_an_itemobject_id
 						) as last_ref_tbl ON cc_io.itemobject_id=last_ref_tbl.bb_io
-			
+
 			SET cc_io.cached_last_ref_date=last_ref_tbl.bb_iv_effective_date,
 				cc_io.cached_last_ref_person=TRIM(CONCAT(last_ref_tbl.bb_iv_user_first_name,' ',last_ref_tbl.bb_iv_user_last_name))
-			{$where1}	
+			{$where1}
 		");
-	}	
+    }
 
-	/**
-	 * Updates the fields itemobject.cached_first_ver_date and itemobject.cached_created_by
-	 * it should be called when a new version is saved.  This query is structured such that
-	 * it assumes there exists a value for each itemobject_id.  None are null.  This lets us use
-	 * the inner join, meaning we will never assign a null value.  If $itemobject_id is specified
-	 * it will only update the cache value for the specific io.  Otherwise, all are updated.
-	 */
-	static public function updateCachedCreatedOnFields($itemobject_id=null) {
-		$where1 = is_null($itemobject_id) ? '' : " WHERE aaa_iv.itemobject_id='{$itemobject_id}'";
-		DbSchema::getInstance()->mysqlQuery("
+    /**
+     * Updates the fields itemobject.cached_first_ver_date and itemobject.cached_created_by
+     * it should be called when a new version is saved.  This query is structured such that
+     * it assumes there exists a value for each itemobject_id.  None are null.  This lets us use
+     * the inner join, meaning we will never assign a null value.  If $itemobject_id is specified
+     * it will only update the cache value for the specific io.  Otherwise, all are updated.
+     */
+    static public function updateCachedCreatedOnFields($itemobject_id = null)
+    {
+        $where1 = is_null($itemobject_id) ? '' : " WHERE aaa_iv.itemobject_id='{$itemobject_id}'";
+        DbSchema::getInstance()->mysqlQuery("
 			UPDATE itemobject as cc_io,
 				# Table of all the first version itemversion info for each itemobject_id
 				 (
@@ -136,87 +142,97 @@ class DBTableRowItemObject extends DBTableRow {
 						  WHERE min_ref_dates.min_effective_date=bb_iv.effective_date
 						  GROUP BY bb_iv.itemobject_id
 						) as first_ver_tbl
-			
+
 			SET cc_io.cached_first_ver_date=first_ver_tbl.bb_iv_effective_date,
 				cc_io.cached_created_by=TRIM(CONCAT(first_ver_tbl.bb_iv_user_first_name,' ',first_ver_tbl.bb_iv_user_last_name))
 			WHERE cc_io.itemobject_id=first_ver_tbl.bb_io
 		");
-	}
-	
-	/**
-	 * returns a nested dictionary of fields given a specific itemobject_id and optionally a date (to know which version should be retrieved)
-	 * 
-	 * @param int $itemobject_id
-	 * @param date string $effective_date if specified, locate the version that was active at or before this date
-	 * @param int $max_depth number of levels to descend
-	 * @param int $level the current level of recursion
-	 * @param array of int $parents breadcrumbs
-	 * @return dictionary of fields values, with subfields expacted as their own dictionaries.
-	 */
-	static function getItemObjectFullNestedArray($itemobject_id,$effective_date=null, $max_depth=null,$level=0,$parents = array()) {
-		$out = array();
-		$ItemVersion = new DBTableRowItemVersion();
-		if ($ItemVersion->getCurrentRecordByObjectId($itemobject_id,$effective_date)) {
-				
-			foreach($ItemVersion->getExportFieldTypes() as $fieldname => $fieldtype) {
-				if (isset($ItemVersion->{$fieldname})) {
-					if (isset($fieldtype['type']) && ($fieldtype['type']=='component')) {
-						if ((is_null($max_depth) || ($max_depth >$level)) && !in_array($ItemVersion->{$fieldname},$parents)) {
-							$new_parents = array_merge($parents, array($itemobject_id));
-							$out[$fieldname] = self::getItemObjectFullNestedArray($ItemVersion->{$fieldname},$effective_date,$max_depth,$level+1,$new_parents);
-						} else {
-							$value_array = $ItemVersion->getComponentValueAsArray($fieldname);
-							$out[$fieldname] = $value_array[$ItemVersion->{$fieldname}];							
-						}
-					} else if ($fieldname=='dictionary_overrides') {
-						$out[$fieldname] = json_decode($ItemVersion->{$fieldname},true);
-					} else {
-						$out[$fieldname] = $ItemVersion->{$fieldname};
-					}
-				}
-			}
-		}
-		return $out;
-	}	
+    }
 
-	/**
-	 * returns a nested dictionary of fields given a specific itemversion_id.  Unlike getItemObjectFullNestedArray this starts with the
-	 * specified itemVERSION_id instead of inferring the version from the itemobject_id and the effective_date.  For sub components, though,
-	 * it will recurse into the getItemObjectFullNestedArray function and use effective dates for determining versions of the components.
-	 * 
-	 * @param int $itemversion_id
-	 * @param int $max_depth number of levels to descend
-	 * @param int $level the current level of recursion
-	 * @param array of int $parents breadcrumbs
-	 * @return dictionary of fields values, with subfields expacted as their own dictionaries.
-	 */
+    /**
+     * returns a nested dictionary of fields given a specific itemobject_id and optionally a date (to know which version should be retrieved)
+     *
+     * @param int $itemobject_id
+     * @param date string $effective_date if specified, locate the version that was active at or before this date
+     * @param int $max_depth number of levels to descend
+     * @param int $level the current level of recursion
+     * @param array of int $parents breadcrumbs
+     * @return dictionary of fields values, with subfields expacted as their own dictionaries.
+     */
+    static function getItemObjectFullNestedArray($itemobject_id, $effective_date = null, $max_depth = null, $level = 0, $parents = array())
+    {
+        $out = array();
+        $ItemVersion = new DBTableRowItemVersion();
+        if ($ItemVersion->getCurrentRecordByObjectId($itemobject_id, $effective_date)) {
+            foreach ($ItemVersion->getExportFieldTypes() as $fieldname => $fieldtype) {
+                $errormsg=array();
+                $ItemVersion->validateFields($ItemVersion->getSaveFieldNames(), $errormsg);
+                if (count($errormsg)>0) {
+                    $out['field_validation_errors'] = $errormsg;
+                }
+                if (isset($ItemVersion->{$fieldname})) {
+                    if (isset($fieldtype['type']) && ($fieldtype['type']=='component')) {
+                        if ((is_null($max_depth) || ($max_depth >$level)) && !in_array($ItemVersion->{$fieldname}, $parents)) {
+                            $new_parents = array_merge($parents, array($itemobject_id));
+                            $out[$fieldname] = self::getItemObjectFullNestedArray($ItemVersion->{$fieldname}, $effective_date, $max_depth, $level+1, $new_parents);
+                        } else {
+                            $value_array = $ItemVersion->getComponentValueAsArray($fieldname);
+                            $out[$fieldname] = $value_array[$ItemVersion->{$fieldname}];
+                        }
+                    } else if ($fieldname=='dictionary_overrides') {
+                        $out[$fieldname] = json_decode($ItemVersion->{$fieldname}, true);
+                    } else {
+                        $out[$fieldname] = $ItemVersion->{$fieldname};
+                    }
+                }
+            }
+        }
+        return $out;
+    }
 
-	static function getItemVersionFullNestedArray($itemversion_id,$max_depth=null,$level=0,$parents = array()) {
-		$out = array();
-		$ItemVersion = new DBTableRowItemVersion();
-		if ($ItemVersion->getRecordById($itemversion_id)) {
-	
-			foreach($ItemVersion->getExportFieldTypes() as $fieldname => $fieldtype) {
-				if (isset($ItemVersion->{$fieldname})) {
-					if (isset($fieldtype['type']) && ($fieldtype['type']=='component')) {
-						if ((is_null($max_depth) || ($max_depth >$level)) && !in_array($ItemVersion->{$fieldname},$parents)) {
-							$new_parents = array_merge($parents, array($ItemVersion->itemobject_id));
-							// we then recurse into getItemObjectFullNestedArray to fetch all the components using effective_dates to determine the version
-							$out[$fieldname] = self::getItemObjectFullNestedArray($ItemVersion->{$fieldname},$ItemVersion->effective_date,$max_depth,$level+1,$new_parents);
-						} else {
-							$value_array = $ItemVersion->getComponentValueAsArray($fieldname);
-							$out[$fieldname] = $value_array[$ItemVersion->{$fieldname}];
-						}
-					} else if ($fieldname=='dictionary_overrides') {
-						$out[$fieldname] = json_decode($ItemVersion->{$fieldname},true);
-					} else {
-						$out[$fieldname] = $ItemVersion->{$fieldname};
-					}
-				}
-			}
-		}
-		return $out;
-	}
-	
-	
+    /**
+     * returns a nested dictionary of fields given a specific itemversion_id.  Unlike getItemObjectFullNestedArray this starts with the
+     * specified itemVERSION_id instead of inferring the version from the itemobject_id and the effective_date.  For sub components, though,
+     * it will recurse into the getItemObjectFullNestedArray function and use effective dates for determining versions of the components.
+     *
+     * @param int $itemversion_id
+     * @param int $max_depth number of levels to descend
+     * @param int $level the current level of recursion
+     * @param array of int $parents breadcrumbs
+     * @return dictionary of fields values, with subfields expacted as their own dictionaries.
+     */
+
+    static function getItemVersionFullNestedArray($itemversion_id, $max_depth = null, $level = 0, $parents = array())
+    {
+        $out = array();
+        $ItemVersion = new DBTableRowItemVersion();
+        if ($ItemVersion->getRecordById($itemversion_id)) {
+            foreach ($ItemVersion->getExportFieldTypes() as $fieldname => $fieldtype) {
+                $errormsg=array();
+                $ItemVersion->validateFields($ItemVersion->getSaveFieldNames(), $errormsg);
+                if (count($errormsg)>0) {
+                    $out['field_validation_errors'] = $errormsg;
+                }
+                if (isset($ItemVersion->{$fieldname})) {
+                    if (isset($fieldtype['type']) && ($fieldtype['type']=='component')) {
+                        if ((is_null($max_depth) || ($max_depth >$level)) && !in_array($ItemVersion->{$fieldname}, $parents)) {
+                            $new_parents = array_merge($parents, array($ItemVersion->itemobject_id));
+                            // we then recurse into getItemObjectFullNestedArray to fetch all the components using effective_dates to determine the version
+                            $out[$fieldname] = self::getItemObjectFullNestedArray($ItemVersion->{$fieldname}, $ItemVersion->effective_date, $max_depth, $level+1, $new_parents);
+                        } else {
+                            $value_array = $ItemVersion->getComponentValueAsArray($fieldname);
+                            $out[$fieldname] = $value_array[$ItemVersion->{$fieldname}];
+                        }
+                    } else if ($fieldname=='dictionary_overrides') {
+                        $out[$fieldname] = json_decode($ItemVersion->{$fieldname}, true);
+                    } else {
+                        $out[$fieldname] = $ItemVersion->{$fieldname};
+                    }
+                }
+            }
+        }
+        return $out;
+    }
+
+
 }
