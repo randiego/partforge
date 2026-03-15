b262d96522a50b85e56a8b74114692bccf057b88
randiego <8w6NycQJ>
2026-03-15 09:34:21 -0700

    Fixed unguarded call with null arg.

--- partforge/application/CsvGenerator.php
+++ partforge/application/CsvGenerator.php
@@ -3,7 +3,7 @@
  *
  * PartForge Enterprise Groupware for recording parts and assemblies by serial number and version along with associated test data and comments.
  *
- * Copyright (C) 2013-2023 Randall C. Black <randy@blacksdesign.com>
+ * Copyright (C) 2013-2026 Randall C. Black <randy@blacksdesign.com>
  *
  * This file is part of PartForge
  *
@@ -53,7 +53,7 @@ class CsvGenerator {
                 if (in_array($field, $passthrough_fields)) {
                     $arr[] = $record[$field];
                 } else {
-                    $arr[] = '"'.str_replace('"', "'", $record[$field]).'"';
+                    $arr[] = '"'.(is_null($record[$field]) ? '' : str_replace('"', "'", $record[$field])).'"';
                 }
             }
                 $out .= implode(',', $arr)."\r\n";
